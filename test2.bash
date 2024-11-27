2012984f733380b43db7b7f2c70ed2284972bac8
Tomoya Tsuji <s23c1093hj@s.chibakoudai.jp>
2024-11-27 09:21:35 +0900

    掛け算コマンドの作成とテスト

--- test2.bash
+++ test2.bash
@@ -1,31 +1,32 @@
 #!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Tomoya Tsuji <s23c1093hj@s.chibakoudai.jp>
 # SPDX-License-Identifier: BSD-3-Clause
+
 ng () {
         echo ${1}行目が違うよ
         res=1
 }
 
 res=0
-### 正常な動作 ###
-out=$(echo 8 | ./prime_factorization)
-[ "${out}" = 8=2*2*2 ] || ng "$LINENO"
 
-out=$(echo 2 | ./prime_factorization)
-[ "${out}" = "2=2, 2は素数だよ" ] || ng "$LINENO"
+### 正常な動作 ###
+out=$(seq 5 | ./multiply)
+[ "${out}" = 120 ] || ng "$LINENO"
 
 ### 異常な動作 ###
-out=$(echo 8.0 | ./prime_factorization)
+out=$(echo 1.0 | ./multiply)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo あ | ./prime_factorization)
+
+out=$(echo あ | ./multiply)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo | ./prime_factorization)
+out=$(echo | ./multiply)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+
+[ "$res" = 0 ] && echo OK
 exit "$res"
